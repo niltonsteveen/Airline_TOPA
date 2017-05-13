f924a9f5c1884eaf9e71b163488bbefded33c8fa
niltonsteveen <nilton.steveen@gmail.com>
2017-05-13 10:56:29 -0500

    camio para probar unit test

--- TOPA/unitTest.py
+++ TOPA/unitTest.py
@@ -1,1 +1,10 @@
-unitTest.py
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        print("hola")
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
