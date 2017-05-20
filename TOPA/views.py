b4abfe20dda8d2e71c02d80f03665d195a846bef
niltonsteveen <nilton.steveen@gmail.com>
2017-05-20 09:11:30 -0500

    se arregla error de sintaxis en el query de views.py en la linea 12

--- TOPA/views.py
+++ TOPA/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Airline, Flight
 from .serializers import AirlineSerializer, FlightSerializer
 from rest_framework.decorators import api_view
+from django.db.models import Q
 # Create your views here.
 
 @api_view(['POST','GET'])
@@ -13,9 +14,8 @@ def ejemplo(request):
 		flights = Flight.objects.filter(flightCode=data['flightCode'], origin=data['origin'], destination=data['destination'],
 			price=data['price'], currency=data['currency'], date=data['date'], roundTrip=data['roundTrip'], passengers=data['passengers'])		
 		if data['roundTrip']:
-			flights = Flight.objects.filter(flightCode=data['flightCode'], origin=data['origin'], destination=data['destination'],
-			price=data['price'], currency=data['currency'], date=data['date'], roundTrip=data['roundTrip'], passengers=data['passengers']) | 
-			Flights.objects.filter(origin=data['destination'], destination=data['origin'])
+			flights = Flight.objects.filter(Q(flightCode=data['flightCode'], origin=data['origin'], destination=data['destination'],
+			price=data['price'], currency=data['currency'], date=data['date'], roundTrip=data['roundTrip'], passengers=data['passengers'])) | Q((origin=data['destination'], destination=data['origin']))
 		serializer = FlightSerializer(flights, many=True)
 		return Response(serializer.data)
 	elif request.method == 'GET':
