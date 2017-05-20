87edbef3e983c6d03973c5d8c11744baa12e7748
jtorresv232 <jtorresv232@gmail.com>
2017-05-20 00:31:14 -0500

    Se realiza test para validar correo e igualmente se hace un test para validar el servicio de obtener todos los vuelos con un GET

--- TOPA/unitTest.py
+++ TOPA/unitTest.py
@@ -1,9 +1,19 @@
 import unittest
+import re
+from . import views
 
 class TestAirline(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_email(self):
+    	email="nilton"
+    	self.assertEqual('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$', email.lower())
+
+    def test_post(self):
+    	data=views.ejemplo
+    	print data.origin
 
 if __name__ == '__main__':
     unittest.main()
