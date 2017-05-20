6b737cbe84839950db57d8aaae2d3d96cadaf9a8
jtorresv232 <jtorresv232@gmail.com>
2017-05-20 01:00:41 -0500

    se hace test para validar correo electronico

--- TOPA/unitTest.py
+++ TOPA/unitTest.py
@@ -1,6 +1,6 @@
 import unittest
 import re
-import views
+#import views
 
 class TestAirline(unittest.TestCase):
 
@@ -11,9 +11,9 @@ class TestAirline(unittest.TestCase):
     	email="nilton"
     	self.assertEqual('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$', email.lower())
 
-    def test_post(self):
-    	data=views.ejemplo
-    	print data.origin
+    #def test_post(self):
+    #	data=views.ejemplo
+    #	print data.origin
 
 if __name__ == '__main__':
     unittest.main()
