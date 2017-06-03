deeb98a5b8dc12999e53801ea58327958f189fde
niltonsteveen <nilton.steveen@gmail.com>
2017-06-03 13:27:38 -0500

    cambio

--- TOPA/views.py
+++ TOPA/views.py
@@ -33,7 +33,8 @@ def setReserve(request):
 		# id_token comes from the client app (shown above)
 		decoded_token = auth.verify_id_token(token)
 		uid = decoded_token['uid']
-		return Response(data={"uid":uid})
+		email=decoded_token['email']
+		return Response(data={"uid":email})
 	elif request.method == 'GET':
 		flights = Flight.objects.all()
 		serializer = FlightSerializer(flights, many=True)
