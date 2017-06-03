658c9d2b5c6334b15f48baa8569fd3e8670a1d7b
niltonsteveen <nilton.steveen@gmail.com>
2017-06-03 01:50:46 -0500

    se agrega post para validar token

--- airline/urls.py
+++ airline/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from TOPA.views import InicioView
-from . import views
+from TOPA import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
