376d1c028d7842464e36dbcb87f4d1bb79568986
niltonsteveen <nilton.steveen@gmail.com>
2017-06-02 20:00:35 -0500

    se añade pagina html para probar funcionalidades de firebase

--- TOPA/urls.py
+++ TOPA/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
 from . import views
-from TOPA.views import InicioView
 
 urlpatterns = [
     url(r'^', views.ejemplo),
-    url(r'^inicio/', InicioView.as_view(), name="inicio"),
 ]
--- airline/urls.py
+++ airline/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from TOPA.views import InicioView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^Search/', include('TOPA.urls')),
+    url(r'^inicio/', InicioView.as_view(), name="inicio"),
 ]
