b13ed1682a73c20c1815c2b940e815dfdd07f4e2
niltonsteveen <nilton.steveen@gmail.com>
2017-06-05 16:08:21 -0500

    unit test de fecha

--- TOPA/unitTest.py
+++ TOPA/unitTest.py
@@ -1,5 +1,7 @@
 import unittest
 import re
+from datetime import datetime, date, time, timedelta
+import calendar
 #import views
 
 class TestAirline(unittest.TestCase):
@@ -7,8 +9,12 @@ class TestAirline(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-	def test_upper(self):
-		self.assertEqual('foof'.upper(), 'FOOF')
+	def test_fecha(self):
+        fecha=date.today() - timedelta(days=2)
+        fechaStr="%d-%m-%Y"
+        cadena=fecha.strftime(fechaStr)
+        esperada="03-06-2017"
+        self.assertEqual(cadena,esperada)
 
 
     #def test_convertirFecha(array):
